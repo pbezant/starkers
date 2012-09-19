baf1eb6b9bff1153d60b33c4d7d8a3ffda9da5ff
Preston Bezant <bezantster@gmail.com>
2012-09-19 09:14:46 -0500

    More files

--- functions.php
+++ functions.php
@@ -97,3 +97,11 @@
 		</li>
 		<?php 
 	}
+
+    if ( function_exists('register_sidebar') )  
+        register_sidebar(array(  
+            'before_widget' => '',  
+            'after_widget' => '',  
+            'before_title' => '<h2>',  
+            'after_title' => '</h2>',  
+        ));  
