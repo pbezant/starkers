976388a1f34b1498b598dff3aad3ff5758160640
pbezant <bezantster@gmail.com>
2012-09-20 00:18:51 -0500

    Added css3 flip http://css3.bradshawenterprises.com/flip/

--- home.php
+++ home.php
@@ -5,21 +5,30 @@ Template Name: Home
 ?>
 
 <?php get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header', 'sidebar') ); ?>
+<style>
+
+</style>
 
 <div id="content-main">
 	<?php $archive_query = new WP_Query('showposts=1000');
 		while ($archive_query->have_posts()) : $archive_query->the_post(); ?>
 			<div id="entry-content" <?php post_class(); ?>>
-			<h2><a href="<?php echo get_permalink(); ?>">
-				<?php if ( has_post_thumbnail() ) {
-					set_post_thumbnail_size( 150, 150, true );
-					the_post_thumbnail();
-				}
-				else
-				the_title(); ?></a>
-			</h2>
-			<?php the_excerpt(); ?>
-		</div>
+			<div id="f1_card">
+				<div class="front face">
+				 	<a href="<?php echo get_permalink(); ?>">
+					<?php if ( has_post_thumbnail() ) {
+						//set_post_thumbnail_size( 150, 150, true );
+						the_post_thumbnail();
+					}
+					else
+						echo "<h2>".get_the_title()."</h2>"; ?>
+					</a>
+				</div>
+				<div class="back face center">
+					<?php the_excerpt(); ?>
+				</div>
+			</div>
+			</div>
 		<?php endwhile; ?>
 
 </div>
