1008be93e61fc3a687ab9cc1a2c65454e9587b87
Preston Bezant <preston@preston-laptop.(none)>
2012-09-19 22:54:38 -0500

    Updated home.php

--- home.php
+++ home.php
@@ -7,10 +7,25 @@ Template Name: Home
 <?php get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header', 'sidebar') ); ?>
 
 <div id="content-main">
+	<?php $archive_query = new WP_Query('showposts=1000');
+		while ($archive_query->have_posts()) : $archive_query->the_post(); ?>
+			<div id="entry-content" <?php post_class(); ?>>
+			<h2><a href="<?php echo get_permalink(); ?>">
+				<?php if ( has_post_thumbnail() ) {
+					set_post_thumbnail_size( 150, 150, true );
+					the_post_thumbnail();
+				}
+				else
+				the_title(); ?></a>
+			</h2>
+			<?php the_content(); ?>
+		</div>
+		<?php endwhile; ?>
+
 	<?php
 	$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 	$args= array(
-		'category_name' => 'Posts, Uncategorized', // Change these category SLUGS to suit your use.
+		'category_name' => 'posts', // Change these category SLUGS to suit your use.
 		'paged' => $paged
 	);
 	query_posts($args);
