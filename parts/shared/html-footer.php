67b25d94725a6b8eb9e01924b3aba6164a05269c
pbezant <bezantster@gmail.com>
2012-09-20 23:55:24 -0500

    Added exerpt length working on adding massonry...

--- parts/shared/html-footer.php
+++ parts/shared/html-footer.php
@@ -4,15 +4,16 @@
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/sidebar.js" type="text/javascript"></script>
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.masonry.min.js" type="text/javascript"></script>
 	<script>
-	jQuery(window).load()(function($) {
+	jQuery(document).load(function($) {
    // put all your jQuery goodness in here.
-   		var entryContent = $('#entry-content').width();
-   		$(function(){
+ 
+
 			$('#content-main').masonry({
 			  itemSelector: '#entry-content',
-			  columnWidth: entryContent
+			   columnWidth: function( containerWidth ) {
+    			return containerWidth / 5;
+  			}
 			});
-		});
 
  	});
 	</script>
