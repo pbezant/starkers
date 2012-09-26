afbdc31eabef97a098e0c9d4c515508ea0e4f48f
pbezant <bezantster@gmail.com>
2012-09-25 23:37:04 -0500

    Isotope Added, Filter Added, Working on gallery page, add custom post templates

--- gallery.php
+++ gallery.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ *	Template Name: Gallery Page 
  * Template Name Posts: gallery
  * The Template for displaying all single posts
  *
@@ -24,10 +25,31 @@
 </div>
 
 
+
 <article>
-	<h2><?php the_title(); ?></h2>
-	<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
-	<?php the_content(); ?>			
+	<?php
+    $gallery_shortcode = '[gallery id="' . intval( $post->post_parent ) . '"]';
+    print apply_filters( 'the_content', $gallery_shortcode );
+ ?>
+	<!--
+	<?php  
+    $args = array(  
+        'numberposts' => -1, // Using -1 loads all posts  
+        'orderby' => 'menu_order', // This ensures images are in the order set in the page media manager  
+        'order'=> 'ASC',  
+        'post_mime_type' => 'image', // Make sure it doesn't pull other resources, like videos  
+        'post_parent' => $post->ID, // Important part - ensures the associated images are loaded 
+        'post_status' => null, 
+        'post_type' => 'attachment'  
+    );  
+    $images = get_children( $args );  
+    if($images){ ?>  
+		<div id="slider">  
+	    <?php foreach($images as $image){ ?>  
+	    <img src="<?php echo $image->guid; ?>" alt="<?php echo $image->post_title; ?>" title="<?php echo $image->post_title; ?>" />  
+	    <?php    } ?>  
+	</div>  
+    <?php } ?>    	
 
 	<?php if ( get_the_author_meta( 'description' ) ) : ?>
 	<?php echo get_avatar( get_the_author_meta( 'user_email' ) ); ?>
@@ -36,8 +58,9 @@
 	<?php endif; ?>
 
 	<?php comments_template( '', true ); ?>
-
+-->
 </article>
+
 <?php endwhile; ?>
 
 <?php get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
