8cb640865fc2b1205238e2cefb88dc656c435f6c
pbezant <bezantster@gmail.com>
2012-10-04 19:14:42 -0500

    stuf

--- functions.php
+++ functions.php
@@ -112,6 +112,7 @@
 	add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 
 	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'custom-background' );
 	add_theme_support( 'post-formats', array( 'aside', 'gallery', 'quote', 'status' ) );
 	
    
